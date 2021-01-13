72ff2d5fc237bb297480c1fef43a4dac131b4693
LogicMason5 <logicmason5@gmail.com>
2021-01-13 13:49:26 +0200

    refactored to use google-maps-react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,36 @@
-
-import React, { useState, useEffect } from 'react'
-
-import Map from './components/map/Map'
+import React, { useState } from 'react'
 import Menu from './components/menu/Menu'
+import Map from './components/map/Map'
 import Messenger from './components/messenger/Messenger'
 import Tabs from '@material-ui/core/Tabs';
 import AppBar from '@material-ui/core/AppBar';
 import Tab from '@material-ui/core/Tab'
+import Box from '@material-ui/core/Box'
 import { Switch, Route, Link } from "react-router-dom";
-import { loadMapApi } from './utils/GoogleMapsUtils'
 
 const App: React.FC = () => {
 
   const [page, setPage] = useState(0);
-  const [scriptLoaded, setScriptLoaded] = useState(false);
-
-  useEffect(() => {
-    const googleMapScript = loadMapApi();
-    googleMapScript.addEventListener('load', function () {
-        setScriptLoaded(true);
-    });
-  }, []);
 
   const handleChange = (event: any, newValue: React.SetStateAction<number>) => {
     setPage(newValue);
   };
 
     return (
-        <div className="App">
+        <Box className="App">
             <AppBar position="static">
-              <Tabs value={page} onChange={handleChange}>
+              <Tabs value={page} onChange={handleChange} variant="fullWidth">
                 <Tab label="Map" component={Link} to="/"/>
                 <Tab label="Messages" component={Link} to="messages"/>
                 <Tab label="Menu" component={Link} to="menu" />
               </Tabs>
-              <Switch>
-                <Route path="/messages" render={() => <Messenger />} />
-                <Route path="/menu" render={() => <Menu />} />
-                <Route path="/" render={() => scriptLoaded && <Map />} />
-              </Switch>
             </AppBar>
-
-        </div>
+            <Switch>
+                <Route path="/messages" render={() => <Messenger />} />
+                <Route path="/menu" render={() => <Map />} />
+                <Route path="/" render={() =>  <Map />} />
+             </Switch>
+        </Box>
     );
   }
 
--- client/src/components/map/Map.tsx
+++ client/src/components/map/Map.tsx
@@ -1,51 +1,29 @@
-import React, { useRef, useState, useEffect } from 'react'
-import { GoogleLatLng, GoogleMap } from '../../types';
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({text}: any) => <div>{text}</div>;
+
+const SimpleMap = (props: any) => {
+  
+    const [center, setCenter] = useState({lat: 60.16211995524449, lng: 24.920254801252657 });
+    const [zoom, setZoom] = useState(11);
 
 
-const Map: React.FC = () => {
-
-  const ref = useRef<HTMLDivElement>(null);
-  const [map, setMap] = useState<GoogleMap>();
-
-  const startMap = (): void => {
-    if (!map) {
-        defaultMapStart();
-    }
-  };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(startMap, [map]);
-
-  const defaultMapStart = (): void => {
-    const defaultAddress = new google.maps.LatLng(60.16211995524449, 24.920254801252657);
-    initMap(10, defaultAddress);
-  };
-
-  const initMap = (zoomLevel: number, adress: GoogleLatLng): void => {
-    if (ref.current) {
-      setMap(
-        new google.maps.Map(ref.current, {
-          zoom: zoomLevel,
-          center: adress,
-          mapTypeControl: false,
-          streetViewControl: false,
-          rotateControl: false,
-          scaleControl: true,
-          fullscreenControl: false,
-          zoomControl: true,
-          gestureHandling: 'cooperative',
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          draggableCursor: 'pointer'
-        })
-      )
-    }
-  }
-
-  return (
-    <div className="map-container" >
-      <div ref={ref} className="map-container_map" style={{ height: '100vh', width: '100%' }}></div>
-    </div>
+    return (
+        <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'add your api key' }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+        >
+          <AnyReactComponent
+            lat={11.0168}
+            lng={76.9558}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
     );
-
 }
 
-export default Map;
+export default SimpleMap;
