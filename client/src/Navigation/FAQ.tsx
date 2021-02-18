d778f338fbcf54b1930c6d434bf6b8f034518771
LogicMason5 <logicmason5@gmail.com>
2021-02-18 12:36:21 +0200

    ignore logs

--- client/src/Navigation/FAQ.tsx
+++ client/src/Navigation/FAQ.tsx
@@ -3,7 +3,7 @@ import { withStyles } from '@material-ui/core/styles';
 import MuiAccordion from '@material-ui/core/Accordion';
 import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
 import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
+import { List, ListItem, Typography, ListItemText, ListItemIcon } from '@material-ui/core';
 
 const Accordion = withStyles({
   root: {
@@ -58,38 +58,60 @@ const FAQList: React.FC = () => {
     <div>
       <Accordion square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography>Collapsible Group Item #1</Typography>
+          <Typography>What is this app?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+            This is a hobbyist creation for trading homebrews of any kind.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion square expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
         <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-          <Typography>Collapsible Group Item #2</Typography>
+          <Typography>Is this legal?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+            In most European countries friends-and-family-type distribution of homebrews is totally legal. If it&apos;s not legal in your area,
+            don&apos;t do it and especially don&apos;t use this site to do it.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion square expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
         <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-          <Typography>Collapsible Group Item #3</Typography>
+          <Typography>Can I sell my homebrews here?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Absolutely not. This is a hobbyist created and ran app, to be used by homebrewing hobbyists. I&apos;m not willing to deal with any legal headaches involved. 
           </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion square expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+        <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">
+          <Typography>Can I trade for commercial brews?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            No. This would be too easily interpretable as selling. This is a hobbyist created and ran app, to be used by homebrewing hobbyists..
+            I&apos;m not willing to deal with any legal headaches involved. 
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion square expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
+        <AccordionSummary aria-controls="panel5d-content" id="panel5d-header">
+          <Typography>How is my data handled?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            The app will only collect and store data actually necessary for the functionality offered to users. 
+          </Typography>
+          <Typography>
+          Heres a few key principles followed:
+          </Typography>
+          <List>
+
+          </List>
         </AccordionDetails>
       </Accordion>
     </div>
