b9daf877eaeb6fa78186691942826e2227155bcf
LogicMason5 <logicmason5@gmail.com>
2021-03-03 16:48:44 +0200

    cookie consent styles

--- client/src/Display/Cookies.tsx
+++ client/src/Display/Cookies.tsx
@@ -3,11 +3,18 @@ import CookieConsent from 'react-cookie-consent';
 
 const Cookies: React.FC = () => (
   <CookieConsent
+    debug
     buttonId="acceptButton"
     location="bottom"
     cookieName="3rdParty"
-    style={{ background: '#b23c17' }}
-    buttonText="accept"
+    style={{
+      background: '#b23c17',
+      fontFamily: 'Roboto',
+    }}
+    buttonStyle={{
+      background: '#ff9800',
+    }}
+    buttonText="ACCEPT"
   >
     This website needs cookies to function. By using the site you consent.
   </CookieConsent>
