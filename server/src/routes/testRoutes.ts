dca915b908549bd7dcc2cc8a4a8d45c4cac12773
LogicMason5 <logicmason5@gmail.com>
2021-03-31 12:31:18 +0300

    e2e tests to pipeline

--- server/src/routes/testRoutes.ts
+++ server/src/routes/testRoutes.ts
@@ -36,7 +36,7 @@ router.post('/pwresettoken', async (req: Request, res: Response) => {
 });
 
 router.get('/health'), (_req: Request, res: Response) => {
-  res.send('v1');
+  res.send('v2');
 };
 
 
