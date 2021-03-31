481f1d95551132cb315de767e04fe71a9b0a47df
LogicMason5 <logicmason5@gmail.com>
2021-03-31 13:22:01 +0300

    v7

--- server/src/routes/testRoutes.ts
+++ server/src/routes/testRoutes.ts
@@ -36,7 +36,7 @@ router.post('/pwresettoken', async (req: Request, res: Response) => {
 });
 
 router.get('/health'), (_req: Request, res: Response) => {
-  res.send('v6');
+  res.send('v7');
 };
 
 
